a0386f9d0950c9dcb83360623f92b41b9c290dbe
selva kumar <selva@selva.local>
2018-08-13 17:14:40 +0530

    initial project setup

---
+++ backend/src/main/java/seam/backend/SeamersApplication.java
@@ -0,0 +1,16 @@
+package seam.backend;
+/**
+ * 
+ * @author Selva
+ *
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeamersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeamersApplication.class, args);
+	}
+}
